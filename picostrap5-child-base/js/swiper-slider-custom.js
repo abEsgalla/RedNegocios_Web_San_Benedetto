3d6f3e6cb4aa468935e059e7659ab88deddf6244
david.perez <david.perez@esgalla.com>
2022-02-08 10:29:46 +0100

    primeros ajustes home

--- picostrap5-child-base/js/swiper-slider-custom.js
+++ picostrap5-child-base/js/swiper-slider-custom.js
@@ -1,28 +1,66 @@
-const swiper_metodo = new Swiper('.swiper-metodo', {
-  // Optional parameters
+const swiper_home = new Swiper('.swiper-home', {
   direction: 'horizontal',
   loop: true,
-  spaceBetween: 15,
-  // Disable preloading of all images
-  preloadImages: false,
-  // Enable lazy loading
-  lazy: true,
-  // If we need pagination
   pagination: {
-    el: '.swiper-pagination-metodo',
+    el: '.swiper-pagination-home',
     clickable: true,
-    bulletActiveClass: 'bg-primary opacity-100',
-  },
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next-metodo',
-    prevEl: '.swiper-button-prev-metodo',
+    bulletActiveClass: 'bg-white opacity-100',
   },
   breakpoints: {
-    // when window width is >= 992px
     992: {
-      loop: true,
-      slidesPerView: 3
+      slidesPerView: 1
+    }
+  }
+});
+
+const swiper_home_dedicado = new Swiper('.swiper-home-dedicado', {
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 55,
+  pagination: {
+    el: '.swiper-pagination-home-dedicado',
+    clickable: true,
+    bulletActiveClass: 'bg-white opacity-100',
+  },
+  breakpoints: {
+    992: {
+      slidesPerView: 4
+    }
+  }
+});
+
+const swiper_home_promocional = new Swiper('.swiper-home-promocional', {
+  direction: 'horizontal',
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination-home-promocional',
+    clickable: true,
+    bulletActiveClass: 'bg-white opacity-100',
+  },
+  breakpoints: {
+    992: {
+      slidesPerView: 1,
+    }
+  }
+});
+
+const swiper_home_noticias = new Swiper('.swiper-home-noticias', {
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 33,
+  breakpoints: {
+    992: {
+      slidesPerView: 4,
+    }
+  }
+});
+
+const swiper_home_marcas = new Swiper('.swiper-home-marcas', {
+  direction: 'horizontal',
+  loop: true,
+  breakpoints: {
+    992: {
+      slidesPerView: 6,
     }
   }
 });
