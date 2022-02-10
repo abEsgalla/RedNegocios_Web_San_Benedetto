ce05aabbd97efe4cb3c68bad7a5a3daf44b61a00
david.perez <david.perez@esgalla.com>
2022-02-10 13:34:36 +0100

    ajustar page marcas dinamica

--- picostrap5-child-base/page-templates/custom-template-marcas.php
+++ picostrap5-child-base/page-templates/custom-template-marcas.php
@@ -4,6 +4,8 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
+$acf_fields = get_fields();
+
 get_header();
 ?>
 
@@ -23,90 +25,9 @@ get_header();
 <section class="py-96">
     <div class="container">
         <div class="row">
-            <div class="col-12">
-                <ul class="justify-content-center nav nav-pills mb-3 h2" id="pills-tab" role="tablist">
-                    <li class="nav-item mx-32" role="presentation">
-                        <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" 
-                        type="button" role="tab" aria-controls="pills-home" aria-selected="true">
-                            AGUA MINERAL
-                        </button>
-                    </li>
-                    <li class="nav-item mx-32" role="presentation">
-                        <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" 
-                        type="button" role="tab" aria-controls="pills-profile" aria-selected="false">
-                            REFRESCOS
-                        </button>
-                    </li>
-                </ul>
-                <div class="row tab-content" id="pills-tabContent">
-                    <div class="col-12 tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
-                        <div class="row text-center">
-                            <div class="col-12 mt-80">
-                                Un producto 100% natural, puro desde su origen y esencial para nuestra salud y bienestar. 
-                                Descubre las propiedades del agua mineral a través de nuestras distintas marcas, 
-                                ¡porque cada agua mineral es única! 
-                            </div>
-                            <div class="col-12">
-                                <div class="row">
-                                    <div class="col-4 mt-64">
-                                        <?=wp_get_attachment_image(52, "full", "", array( 'class' => '' , 'alt' => 'Banner Home Slider' , 'title' => 'Banner Home Slider') ); ?>
-                                        <div class="text-start mt-20">test</div>
-                                    </div>
-                                    <div class="col-4 mt-64">
-                                        <?=wp_get_attachment_image(52, "full", "", array( 'class' => '' , 'alt' => 'Banner Home Slider' , 'title' => 'Banner Home Slider') ); ?>
-                                        <div class="text-start mt-20">test</div>
-                                    </div>
-                                    <div class="col-4 mt-64">
-                                        <?=wp_get_attachment_image(52, "full", "", array( 'class' => '' , 'alt' => 'Banner Home Slider' , 'title' => 'Banner Home Slider') ); ?>
-                                        <div class="text-start mt-20">test</div>
-                                    </div>
-                                    <div class="col-4 mt-64">
-                                        <?=wp_get_attachment_image(52, "full", "", array( 'class' => '' , 'alt' => 'Banner Home Slider' , 'title' => 'Banner Home Slider') ); ?>
-                                        <div class="text-start mt-20">test</div>
-                                    </div>
-                                    <div class="col-4 mt-64">
-                                        <?=wp_get_attachment_image(52, "full", "", array( 'class' => '' , 'alt' => 'Banner Home Slider' , 'title' => 'Banner Home Slider') ); ?>
-                                        <div class="text-start mt-20">test</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
-                        <div class="row text-center">
-                            <div class="col-12 mt-80">
-                                Un producto 100% natural, puro desde su origen y esencial para nuestra salud y bienestar. 
-                                Descubre las propiedades del agua mineral a través de nuestras distintas marcas, 
-                                ¡porque cada agua mineral es única! 
-                            </div>
-                            <div class="col-12">
-                                <div class="row">
-                                    <div class="col-4 mt-64">
-                                        <?=wp_get_attachment_image(52, "full", "", array( 'class' => '' , 'alt' => 'Banner Home Slider' , 'title' => 'Banner Home Slider') ); ?>
-                                        <div class="text-start mt-20">test</div>
-                                    </div>
-                                    <div class="col-4 mt-64">
-                                        <?=wp_get_attachment_image(52, "full", "", array( 'class' => '' , 'alt' => 'Banner Home Slider' , 'title' => 'Banner Home Slider') ); ?>
-                                        <div class="text-start mt-20">test</div>
-                                    </div>
-                                    <div class="col-4 mt-64">
-                                        <?=wp_get_attachment_image(52, "full", "", array( 'class' => '' , 'alt' => 'Banner Home Slider' , 'title' => 'Banner Home Slider') ); ?>
-                                        <div class="text-start mt-20">test</div>
-                                    </div>
-                                    <div class="col-4 mt-64">
-                                        <?=wp_get_attachment_image(52, "full", "", array( 'class' => '' , 'alt' => 'Banner Home Slider' , 'title' => 'Banner Home Slider') ); ?>
-                                        <div class="text-start mt-20">test</div>
-                                    </div>
-                                    <div class="col-4 mt-64">
-                                        <?=wp_get_attachment_image(52, "full", "", array( 'class' => '' , 'alt' => 'Banner Home Slider' , 'title' => 'Banner Home Slider') ); ?>
-                                        <div class="text-start mt-20">test</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <?php
+              get_template_part( 'template-parts/marcas/seccion', 'slider_productos', $acf_fields['slider_productos']);
+            ?>
         </div>
     </div>
 </section>
---
+++ picostrap5-child-base/template-parts/marcas/seccion-slider_productos.php
@@ -0,0 +1,71 @@
+<?php
+  $fields = wp_parse_args( $args );
+
+  $category_repeater=array();
+  $categories_id_slider_html=array();
+
+  foreach ($fields as $id_producto):
+    $category_id=get_the_terms( $id_producto, 'categorias_producto' )[0]->term_id;
+    if (!in_array($category_id, $category_repeater)):
+      if(!array_key_exists($category_id, $categories_id_slider_html)) {
+        $categories_id_slider_html[$category_id]=array();
+      }
+      array_push($categories_id_slider_html[$category_id], $id_producto);
+    endif;
+  endforeach;
+
+  $counter=0;
+  $cabecera_tabs_html="";
+  $cuerpo_tabs_html="";
+
+  foreach ($categories_id_slider_html as $id_term => $id_posts):
+    $term_name = get_term( $id_term )->name;
+    $term_name_sanitice = str_replace(' ', '-', strtolower($term_name));
+    $active=($counter==0)?'active':'';
+    $show=($counter==0)?'show':'';
+    $selected=($counter==0)?'true':'false';
+    $cabecera_tabs_html.="
+      <li class='nav-item mx-32' role='presentation'>
+          <button class='nav-link ".$active."' id='".$term_name_sanitice."-tab' 
+          data-bs-toggle='pill' data-bs-target='#".$term_name_sanitice."' 
+          type='button' role='tab' aria-controls='".$term_name_sanitice."' aria-selected='".$selected."'>
+          ".strtoupper($term_name)."
+          </button>
+      </li>";
+    $cuerpo_tabs_html.="
+    <div class='col-12 tab-pane fade ".$show." ".$active."' id='".$term_name_sanitice."' 
+    role='tabpanel' aria-labelledby='".$term_name_sanitice."-tab'>
+      <div class='row text-center'>
+          <div class='col-12 mt-80'>
+              ".get_term_field( 'description', $id_term )."
+          </div>
+          <div class='col-12'>
+              <div class='row'>";
+    foreach ($id_posts as $id_producto):
+    $cuerpo_tabs_html.="
+                <div class='col-4 mt-64'>
+                    ".wp_get_attachment_image(get_field('imagen_producto',$id_producto), 'full', '', array( 'class' => '' , 'alt' => 'Banner Home Slider' , 'title' => 'Banner Home Slider') )."
+                    <div class='text-start mt-20'>".strtoupper(get_the_title($id_producto))."</div>
+                </div>
+    ";
+    endforeach;
+    $cuerpo_tabs_html.="
+              </div>
+          </div>
+      </div>
+    </div>
+    ";
+  $counter++;
+  endforeach;
+  unset($counter);
+
+?>
+
+<div class="col-12">
+  <ul class="justify-content-center nav nav-pills mb-3 h2" id="pills-tab" role="tablist">
+      <?=$cabecera_tabs_html?>
+  </ul>
+  <div class="row tab-content" id="pills-tabContent">
+      <?=$cuerpo_tabs_html?>
+  </div>
+</div>
