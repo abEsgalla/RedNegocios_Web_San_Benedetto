7795686b8f8190b3779e4bbc6b291db29d179237
david.perez <david.perez@esgalla.com>
2022-02-18 14:15:15 +0100

    template mision & valores

---
+++ picostrap5-child-base/page-templates/custom-template-valores.php
@@ -0,0 +1,232 @@
+<?php
+/* Template Name: Page Valores */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+$acf_fields = get_fields();
+
+get_header();
+?>
+
+<section>
+  <div class="position-relative bg-black">
+    <?=wp_get_attachment_image(606, "full", "", array( 'class' => 'opacity-50' , 'alt' => 'Banner Home Slider' , 'title' => 'Banner Home Slider') ); ?>
+  </div>
+  <div class="container-fluid zi-99 top-50 position-absolute transform-translate-y">
+    <div class="container">
+      <div class="row">
+        <div class="col-4">
+          <div class="row text-white text-uppercase">
+            <div class="col-12">
+              GRUPO SAN BENEDETTO
+            </div>
+            <h1 class="col-12 fs-64 fw-bold text-primary">
+              Visión
+            </h1>
+            <div class="col-12 mt-48">
+              Ser la Total Beverage Company que eligen las personas para hidratarse y refrescarse, 
+              donde quieran y cuando quieran, a través de bebidas que saben bien y hacen sentir bien.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="my-96">
+  <div class="container position-relative py-96">
+    <div class="row">
+    
+      <div style="" class="g-0 col-12 col-sm-6 d-block bg-primary">
+        <div class="row position-relative h-100">
+          <div class="container-fluid zi-99 top-50 position-absolute transform-translate-y">
+            <div class="container">
+              <div class="row">
+                <div class="col-12">
+                  <div class="row text-secondary">
+                    <div class="col-12 text-uppercase">
+                      ésta es Nuestra
+                    </div>
+                    <div class="col-12 fs-64 fw-bold">
+                      Misión
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div style="" class="g-0 col-12 col-sm-6 d-block">
+        <div class="row position-relative">
+          <div class="position-relative transform-translate-ny">
+            <?=wp_get_attachment_image(607, "full", "", array( 'class' => 'w-100' , 'alt' => 'Banner Home Slider' , 'title' => 'Banner Home Slider') ); ?>
+          </div>
+          <div class="container-fluid zi-99 top-50 position-absolute transform-translate-y">
+            <div class="container">
+              <div class="row">
+                <div class="col-12">
+                  <div class="row text-secondary">
+                    <div class="col-12 text-uppercase h4">
+                      «Integrar calidad, seguridad y capacidad de innovación con un profundo respeto por el medio ambiente, 
+                      el territorio y el desarrollo de las personas.»  
+                    </div>
+                    <div class="col-12">
+                      ― Enrico Zoppas, Presidente de Gruppo Acqua Minerale San Benedetto S.p.A. 
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div style="" class="g-0 col-12 col-sm-6 d-block">
+        <div class="row position-relative">
+          <div class="position-relative">
+            <?=wp_get_attachment_image(610, "full", "", array( 'class' => 'w-100' , 'alt' => 'Banner Home Slider' , 'title' => 'Banner Home Slider') ); ?>
+          </div>
+        </div>
+      </div>
+
+      <div style="" class="g-0 col-12 col-sm-6 d-block">
+        <div class="row position-relative">
+          <div class="position-relative">
+            <?=wp_get_attachment_image(609, "full", "", array( 'class' => 'w-100' , 'alt' => 'Banner Home Slider' , 'title' => 'Banner Home Slider') ); ?>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+<section class="my-96">
+  <div class="container">
+    <div class="row text-secondary">
+      <div class="col-12 text-uppercase text-center">
+        descubre nuestros
+      </div>
+      <div class="col-12 fs-64 fw-bold text-center">
+        Valores
+      </div>
+      <div class="col-12">
+        <div class="row">
+          <div class="col-12">
+            <div class="row">
+              <div class="col-6 mt-80">
+                <div class="row">
+                  <div class="col-12">
+                    01
+                  </div>
+                  <div class="col-12">
+                    Liderazgo en calidad.
+                  </div>
+                  <div class="col-12">
+                    Nuestra máxima prioridad es poder garantizar una oferta de productos asequibles, 
+                    capaz de satisfacer las distintas necesidades de consumo y con los mejores estándares de calidad. 
+                    Somos la primera envasadora de agua mineral en España y uno de los principales fabricantes de refrescos del país: 
+                    nuestra gama de productos cubre la mayor parte del segmento de bebidas analcohólicas, 
+                    y lo hace bajo los máximos niveles de seguridad alimentaria.
+                  </div>
+                </div>
+              </div>
+              <div class="col-6 mt-80">
+                <div class="row">
+                  <div class="col-12">
+                    02
+                  </div>
+                  <div class="col-12">
+                    Innovación e investigación.
+                  </div>
+                  <div class="col-12">
+                    Si algo nos ha caracterizado desde nuestros orígenes es un profundo espíritu visionario, 
+                    anticipándonos siempre a las grandes tendencias del futuro que revolucionarían el mercado. 
+                    Primero fue la introducción de la botella de PET en 1980, 
+                    después la patente de tapones push&pull completamente asépticos; más tarde, 
+                    la medición y neutralización de emisiones de CO2. 
+                    Un espíritu que nos sigue guiando hoy en día en nuestro camino hacia una Total Beverage Company. 
+                  </div>
+                </div>
+              </div>
+              <div class="col-6 mt-80">
+                <div class="row">
+                  <div class="col-12">
+                    03
+                  </div>
+                  <div class="col-12">
+                    Responsabilidad con la comunidad.
+                  </div>
+                  <div class="col-12">
+                    Formamos parte de las empresas que ofrecen soluciones a la España vaciada. 
+                    Las envasadoras de agua mineral generamos empleo de calidad en zonas rurales, 
+                    contribuyendo además al crecimiento económico local. Además, 
+                    en Agua Mineral San Benedetto colaboramos de forma activa 
+                    con las iniciativas socio-culturales de nuestro entorno más cercano.
+                  </div>
+                </div>
+              </div>
+              <div class="col-6 mt-80">
+                <div class="row">
+                  <div class="col-12">
+                    04
+                  </div>
+                  <div class="col-12">
+                    Bienestar laboral.
+                  </div>
+                  <div class="col-12">
+                    En Agua Mineral San Benedetto queremos atraer talento, 
+                    pero más nos preocupa ser capaces de fidelizarlo. 
+                    Por eso nos enorgullece poder presumir de la media de antigüedad entre nuestros empleados, 
+                    que ronda los 16 años. Y es que, al incorporarse a nuestra plantilla, 
+                    los trabajadores no sólo acceden a múltiples oportunidades de formación y promoción: 
+                    también se suman a un proyecto de familia, con una cultura preventiva del cuidado de la persona, 
+                    respetuosa, abierta a la comunicación bidireccional, diversa e igualitaria. 
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>  
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="my-96">
+  <div class="container-fluid">
+    <div class="row position-relative">
+      <div class="col-6 g-0 bg-black position-relative">
+        <div class="position-relative">
+          <?=wp_get_attachment_image(611, "full", "", array( 'class' => 'w-100 opacity-75' , 'alt' => 'Banner Home Slider' , 'title' => 'Banner Home Slider') ); ?>
+        </div>
+      </div>
+      <div class="col-6 text-secondary bg-primary d-flex align-items-center">
+        Desde el principio nos hemos preocupado por ser capaces de desarrollar nuestra actividad con el mínimo impacto 
+        en nuestro entorno. Lo conseguimos a través de proyectos y sinergias estratégicas con agentes locales. 
+        Así, no sólo fabricamos los envases más ligeros del mercado de agua mineral: 
+        también aprovechamos al máximo nuestros recursos hídricos, evitando su desperdicio; 
+        fomentamos la circularidad de nuestros envases y residuos, 
+        realizamos tareas de limpieza y reforestación en nuestros alrededores, etc. ¡Descubre más en nuestra Ecolosofía! 
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="my-96">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 position-relative">
+        <?php
+            get_template_part( 'template-parts/general/seccion', 'slider_logos', get_field('slider_logos', 'options'));
+        ?>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php get_footer();
