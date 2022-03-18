a63e324fc7097989b5326336240e6c50025634bb
david.perez <david.perez@esgalla.com>
2022-03-18 13:32:21 +0100

    fix cabecera efectos

--- picostrap5-child-base/js/efectos-cabecera.js
+++ picostrap5-child-base/js/efectos-cabecera.js
@@ -35,41 +35,95 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 
+
   //OFFCANVAS MENU
-  var menuContainerColor = document.querySelector("#wrapper-navbar .zi-1050");
+    var menuContainerColor = document.querySelector("#wrapper-navbar .zi-1050");
 
-  var menuMarcasOffcanvas = document.getElementById('offcanvas-marcas');
-  showMenuElement(menuMarcasOffcanvas);
+    async function waitUntil(condition) {
+      return await new Promise(resolve => {
+        const interval = setInterval(() => {
+          if (condition) {
+            resolve();
+            clearInterval(interval);
+            canvasMenu();
+          };
+        }, 1000);
+      });
+    }
+    waitUntil(typeof bootstrap != 'undefined');
 
-  var menuNosotrosOffcanvas = document.getElementById('offcanvas-nosotros');
-  showMenuElement(menuNosotrosOffcanvas);
+    function canvasMenu(){
+      generate_menuCanvas('nosotros');
+      generate_menuCanvas('marcas');
+      generate_menuCanvas('ecolosofia');
+      generate_menuCanvas('contacto');
+    }
 
-  var menuNosotrosOffcanvas = document.getElementById('offcanvas-ecolosofia');
-  showMenuElement(menuNosotrosOffcanvas);
+    var custom_bootstrap = {};
 
-  var menuNosotrosOffcanvas = document.getElementById('offcanvas-contacto');
-  showMenuElement(menuNosotrosOffcanvas);
+    function generate_menuCanvas(id_element){
+      eval( "var linkMenu"+id_element+" = document.querySelector('[data-bs-target=\"#offcanvas-"+id_element+"\"]');");
+      eval( "var menuCanvas"+id_element+"Offcanvas = document.getElementById('offcanvas-"+id_element+"');");
+      eval( "var event"+id_element+"Offcanvas = new bootstrap.Offcanvas(menuCanvas"+id_element+"Offcanvas);");
 
-  function showMenuElement(element){
-    element.addEventListener('show.bs.offcanvas', function (event) {
-      event.relatedTarget.classList.add("active","active-offcanvas");
-      menuContainerColor.classList.add("menu-open-offcanvas");
-      element.parentElement.classList.remove("h-0");
-      element.classList.remove("h-0");
-      element.classList.add("h-auto");
+      eval("linkMenu"+id_element).addEventListener('mouseover', function (event) {
+        checkAllMenuOffcanvasClose(true);
+
+        eval( "custom_bootstrap."+id_element+" = event"+id_element+"Offcanvas;");
+        eval("linkMenu"+id_element).classList.add("active","active-offcanvas");
+        eval("event"+id_element+"Offcanvas").show();
+        eval("menuCanvas"+id_element+"Offcanvas").classList.add("menu-open-offcanvas");
+        eval("menuCanvas"+id_element+"Offcanvas").parentElement.classList.remove("h-0");
+        eval("menuCanvas"+id_element+"Offcanvas").classList.remove("h-0");
+        eval("menuCanvas"+id_element+"Offcanvas").classList.add("h-auto");
+
+      });
+      eval("menuCanvas"+id_element+"Offcanvas").addEventListener('show.bs.offcanvas', function () {
+        //eval("menuCanvas"+id_element+"Offcanvas").classList.remove("d-none");
+      });
+      eval("menuCanvas"+id_element+"Offcanvas").addEventListener('hidden.bs.offcanvas', function () {
+        eval("menuCanvas"+id_element+"Offcanvas").classList.remove("d-none");
+        if(document.querySelector(".active.active-offcanvas")){
+          document.querySelector(".active.active-offcanvas").classList.remove("active");
+        }
+        eval("menuCanvas"+id_element+"Offcanvas").classList.remove("h-auto");
+        eval("menuCanvas"+id_element+"Offcanvas").classList.add("h-0");
+        menuContainerColor.classList.remove("menu-open-offcanvas");
+      });
+    }
+
+    function checkAllMenuOffcanvasClose(d_none){
+      checkMenuOffcanvasClose('nosotros',d_none);
+      checkMenuOffcanvasClose('marcas',d_none);
+      checkMenuOffcanvasClose('ecolosofia',d_none);
+      checkMenuOffcanvasClose('contacto',d_none);
+    }
+
+    function checkMenuOffcanvasClose(id_element,d_none){
+      if(eval("custom_bootstrap."+id_element)){
+        if(eval("custom_bootstrap."+id_element+"._isShown")==true){
+          if(document.querySelector(".active.active-offcanvas")){
+            document.querySelector(".active.active-offcanvas").classList.remove("active");
+          }
+          if(d_none==true){
+            eval("custom_bootstrap."+id_element+"._element.classList.add('d-none');");
+          }
+          eval("custom_bootstrap."+id_element).hide();
+        }
+      }
+    }
+
+    document.querySelector('.fixed-top').addEventListener('mouseleave', function (event) {
+      checkAllMenuOffcanvasClose(false);
     });
-    element.addEventListener('hide.bs.offcanvas', function () {
-      /*element.classList.add("h-0");
-      element.classList.remove("h-auto");*/
+
+    const navs_link_not_offcanvas = document.querySelectorAll('.fixed-top .nav-link:not([role="button"])');
+    navs_link_not_offcanvas.forEach(function(nav_link_not_offcanvas) {
+      nav_link_not_offcanvas.addEventListener('mouseover', function (event) {
+        checkAllMenuOffcanvasClose(false);
+      });
     });
-    element.addEventListener('hidden.bs.offcanvas', function () {
-      //element.parentElement.classList.add("h-0");
-      document.querySelector(".active.active-offcanvas").classList.remove("active");
-      element.classList.remove("h-auto");
-      element.classList.add("h-0");
-      menuContainerColor.classList.remove("menu-open-offcanvas");
-    });
-  }
+  //OFFCANVAS MENU//
 
   //LANDING PRODUCTOS DATOS SLIDER
   var sliderProductosOffcanvas = document.getElementById('offcanvasInfoSliderProductos');
