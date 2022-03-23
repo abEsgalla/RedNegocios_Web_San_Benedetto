c1c6f5ee89ac3459ca6203563084fc096740bc62
david.perez <david.perez@esgalla.com>
2022-03-23 15:38:46 +0100

    ajuste page valores

--- picostrap5-child-base/page-templates/custom-template-valores.php
+++ picostrap5-child-base/page-templates/custom-template-valores.php
@@ -9,7 +9,7 @@ $acf_fields = get_fields();
 get_header();
 ?>
 
-<section>
+<section class="text-vision">
   <div class="container-fluid position-relative zi-9 bg-black pt-270 pb-317">
     <div class="bg-custom-image opacity-60 w-100 h-100 position-absolute top-0 start-0" 
       style="background:url('<?=wp_get_attachment_image_url(606, "full")?>')"></div>
@@ -38,7 +38,7 @@ get_header();
   <div class="container position-relative">
     <div class="row">
     
-      <div style="" class="g-0 col-12 col-sm-6 d-block bg-primary-brighter">
+      <div style="" class="text-mision g-0 col-12 col-sm-6 d-block bg-primary-brighter">
         <div class="row position-relative h-100">
           <div class="container-fluid zi-99 top-50 position-absolute transform-translate-y">
             <div class="container">
@@ -62,7 +62,9 @@ get_header();
       <div style="" class="g-0 col-12 col-sm-6 d-block">
         <div class="row position-relative">
           <div class="position-relative transform-translate-ny">
-            <?=wp_get_attachment_image(607, "full", "", array( 'class' => 'w-100' , 'alt' => '' , 'title' => '') ); ?>
+            <img src="<?=get_stylesheet_directory_uri().'/src/img/background_nuestro_compromiso.png'?>" 
+            class="d-none d-xxxl-block position-absolute top-0 end-0 w-auto translate-dots-col-after opacity-60">
+            <?=wp_get_attachment_image(607, "full", "", array( 'class' => 'w-100 position-relative' , 'alt' => '' , 'title' => '') ); ?>
           </div>
           <div class="container-fluid zi-99 top-50 position-absolute transform-translate-y">
             <div class="container">
@@ -86,13 +88,17 @@ get_header();
 
       <div style="" class="g-0 col-12 col-sm-6">
         <div class="position-relative d-flex w-100 h-100">
-          <?=wp_get_attachment_image(610, "full", "", array( 'class' => 'center-image w-100' , 'alt' => '' , 'title' => '') ); ?>
+          <?=wp_get_attachment_image(610, "full", "", array( 'class' => 'position-relative zi-99 center-image w-100' , 'alt' => '' , 'title' => '') ); ?>
+          <img src="<?=get_stylesheet_directory_uri().'/src/img/background_nuestro_compromiso.png'?>" 
+            class="d-none d-xxxl-block position-absolute bottom-0 start-0 w-auto translate-dots-col-before opacity-60">
         </div>
       </div>
 
       <div style="" class="g-0 col-12 col-sm-6">
         <div class="position-relative d-flex w-100 h-100">
-          <?=wp_get_attachment_image(609, "full", "", array( 'class' => 'center-image w-100' , 'alt' => '' , 'title' => '') ); ?>
+          <?=wp_get_attachment_image(609, "full", "", array( 'class' => 'position-relative zi-99 center-image w-100' , 'alt' => '' , 'title' => '') ); ?>
+          <img src="<?=get_stylesheet_directory_uri().'/src/img/background_nuestro_compromiso.png'?>" 
+            class="d-none d-xxxl-block position-absolute bottom-0 end-0 w-auto translate-dots-col-bottom-after opacity-60">
         </div>
       </div>
 
