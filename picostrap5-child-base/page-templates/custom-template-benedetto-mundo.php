f93a4a0aac9beec4080764ad6994c7e14a808a4b
david.perez <david.perez@esgalla.com>
2022-03-23 11:16:00 +0100

    fix cabecera benedetto mundo

--- picostrap5-child-base/page-templates/custom-template-benedetto-mundo.php
+++ picostrap5-child-base/page-templates/custom-template-benedetto-mundo.php
@@ -9,31 +9,30 @@ $acf_fields = get_fields();
 get_header();
 ?>
 
-<section>
-  <div class="position-relative bg-black">
-    <?=wp_get_attachment_image(669, "full", "", array( 'class' => 'opacity-50' , 'alt' => '' , 'title' => '') ); ?>
-  </div>
-  <div class="container-fluid zi-99 top-50 position-absolute transform-translate-y">
-    <div class="container">
-      <div class="row">
-        <div class="col-6">
-          <div class="row text-white">
-            <div class="col-12 fs-18">
-              GRUPO SAN BENEDETTO
-            </div>
-            <h1 class="col-12 h2">
-              Agua Mineral San Benedetto en el mundo
-            </h1>
-            <div class="col-12 fs-24 mt-48 text-uppercase fw-bold">
-              Agua Mineral San Benedetto es una 
-              empresa perteneciente al <span class="text-decoration-underline">Gruppo 
-              Acqua Minerale San Benedetto S.p.A. </span>
+<section class="">
+    <div class="container-fluid position-relative zi-99 bg-black pt-270 pb-254">
+        <div class="bg-custom-image opacity-60 w-100 h-100 position-absolute top-0 start-0" 
+        style="background:url('<?=wp_get_attachment_image_url(669, "full")?>')"></div>
+        <div class="container position-relative">
+          <div class="row">
+            <div class="col-6">
+              <div class="row text-white">
+                <div class="col-12 fs-18">
+                  GRUPO SAN BENEDETTO
+                </div>
+                <h1 class="col-12 h2">
+                  Agua Mineral San Benedetto en el mundo
+                </h1>
+                <div class="col-12 fs-24 mt-48 text-uppercase fw-bold">
+                  Agua Mineral San Benedetto es una 
+                  empresa perteneciente al <span class="text-decoration-underline">Gruppo 
+                  Acqua Minerale San Benedetto S.p.A. </span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
     </div>
-  </div>
 </section>
 
 <section class="my-96 mt-xl-160 mb-xl-0">
@@ -54,7 +53,7 @@ get_header();
           gracias a su know-how y distribuye en más de 100 países en todo el mundo.
         </div>
       </div>
-      <div class="offset-2 col-6 position-relative">
+      <div class="offset-2 col-6 position-relative zi-99">
         <?=wp_get_attachment_image(926, "full", "", array( 'class' => 'position-absolute w-100 top-0 transform-translate-ny' , 'alt' => '' , 'title' => '') ); ?>
       </div>
     </div>
