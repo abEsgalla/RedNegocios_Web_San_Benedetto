093eea0be08adb8a774cc163d7f2f2e6d2da0bfc
Alejandro Bernardez <alejandro.bernardez@esgalla.com>
2022-03-24 19:35:48 +0100

    Mod - Modificación de cabeceras de plantilla de landings

--- picostrap5-child-base/template-parts/landing/productos/template-essenzia.php
+++ picostrap5-child-base/template-parts/landing/productos/template-essenzia.php
@@ -1,6 +1,7 @@
 <?  $acf_fields = get_fields();
     $id_producto=get_the_ID();
 ?>
+
 <section>
     <div class="container-fluid position-relative zi-99">
         <div class="bg-custom-image w-100 h-100 position-absolute top-0 start-0" 
@@ -8,7 +9,7 @@
         <div class="container position-relative">
             <div class="row">
                 <div class="col-12 text-center">
-                        <?=wp_get_attachment_image($acf_fields['cabecera']['producto_central'], "full", "", array( 'class' => '' , 'alt' => '' , 'title' => '') ); ?>
+                    <?=wp_get_attachment_image($acf_fields['cabecera']['producto_central'], "full", "", array( 'class' => 'mtn-custom-25 producto-central' , 'alt' => '' , 'title' => '') ); ?>
                 </div>
             </div>
         </div>
