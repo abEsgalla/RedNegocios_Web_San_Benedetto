e9f4e0a301b30119b4af54c5a768ec07781d3cb1
ivan.perez <ivan.perez@esgalla.com>
2022-03-24 18:41:26 +0100

    Efectos

--- picostrap5-child-base/template-parts/landing/productos/template-essenzia.php
+++ picostrap5-child-base/template-parts/landing/productos/template-essenzia.php
@@ -139,7 +139,7 @@
                             foreach ($seccion['col'] as $col): ?>
                                 <div class="col-12 position-relative zi-9 <?=$col['tamano']?> columna-<?=$i ?>">
                                 <? if($col['posicion_texto'] && $col['texto']): ?>
-                                        <div class="text-start text-secondary texto px-lg-48 mb-70">
+                                        <div class="text-start text-secondary texto px-lg-48 mb-70" data-aos="fade-left">
                                         <? if($col['posicion_texto']): ?>
                                             <?=$col['texto']?>
                                         <? endif; ?>
