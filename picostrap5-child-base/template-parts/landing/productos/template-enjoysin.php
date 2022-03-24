903e1505c3462eee00e9797351422b7303be715e
Alejandro Bernardez <alejandro.bernardez@esgalla.com>
2022-03-24 18:07:45 +0100

    Landings

--- picostrap5-child-base/template-parts/landing/productos/template-enjoysin.php
+++ picostrap5-child-base/template-parts/landing/productos/template-enjoysin.php
@@ -1,28 +1,24 @@
 <?  $acf_fields = get_fields();
     $id_producto=get_the_ID();
 ?>
+
 <section>
-    <div class="container-fluid g-0 position-relative">
-        <div class="row g-0">
-            <div class="col-12">
-                <?php if(isset($acf_fields['cabecera']['fondo'])): ?>
-                <div class="position-relative w-cabecera-animada w-cabecera-esjoy">
-                    <img src="<?=get_stylesheet_directory_uri().'/src/img/background-esjoy/fresa_1.png'; ?>" class="position-absolute fresa-1">
-                    <img src="<?=get_stylesheet_directory_uri().'/src/img/background-esjoy/fresa_2.png'; ?>" class="position-absolute fresa-2">
-                    <img src="<?=get_stylesheet_directory_uri().'/src/img/background-esjoy/limon_1.png'; ?>" class="position-absolute limon-1">						
-                    <img src="<?=get_stylesheet_directory_uri().'/src/img/background-esjoy/limon_3.png'; ?>" class="position-absolute limon-3">
-                    <img src="<?=get_stylesheet_directory_uri().'/src/img/background-esjoy/limon_4.png'; ?>" class="position-absolute limon-4">
-                    <?=wp_get_attachment_image($acf_fields['cabecera']['fondo'], "full", "", array( 'class' => 'w-100' , 'alt' => '' , 'title' => '') ); ?>
-                    <?php if(isset($acf_fields['cabecera']['producto_central'])): ?>
-                    <div class="d-flex align-items-center justify-content-center container-fluid zi-99 top-0 position-absolute h-100">
-                        <?=wp_get_attachment_image($acf_fields['cabecera']['producto_central'], "full", "", array( 'class' => 'd-block w-auto h-100 producto-central' , 'alt' => '' , 'title' => '') ); ?>
-                    </div>
-                    <?php endif; ?>
-                </div>
-                <?php endif; ?>
-            </div>
-        </div>
-    </div>
+	<div class="container-fluid position-relative zi-99">
+		<div class="bg-custom-image w-100 h-100 position-absolute top-0 start-0" 
+		style="background:url('<?=wp_get_attachment_image_url($acf_fields['cabecera']['fondo'], "full")?>')"></div>
+		<div class="container position-relative">
+			<div class="row">
+				<div class="col-12 text-center">
+						<?=wp_get_attachment_image($acf_fields['cabecera']['producto_central'], "full", "", array( 'class' => 'mtn-custom-25' , 'alt' => '' , 'title' => '') ); ?>
+				</div>
+			</div>
+		</div>
+        <img src="<?=get_stylesheet_directory_uri().'/src/img/background-esjoy/fresa_1.png'; ?>" class="position-absolute fresa-1">
+        <img src="<?=get_stylesheet_directory_uri().'/src/img/background-esjoy/fresa_2.png'; ?>" class="position-absolute fresa-2">
+        <img src="<?=get_stylesheet_directory_uri().'/src/img/background-esjoy/limon_1.png'; ?>" class="position-absolute limon-1">						
+        <img src="<?=get_stylesheet_directory_uri().'/src/img/background-esjoy/limon_3.png'; ?>" class="position-absolute limon-3">
+        <img src="<?=get_stylesheet_directory_uri().'/src/img/background-esjoy/limon_4.png'; ?>" class="position-absolute limon-4">
+	</div>
 </section>
 
 <?php
