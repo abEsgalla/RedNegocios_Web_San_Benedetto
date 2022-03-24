6f3c0c977ab4624ea390e2e7478e1ccd49930a27
ivan.perez <ivan.perez@esgalla.com>
2022-03-24 15:37:33 +0100

    Borrar coments

--- picostrap5-child-base/js/efectos-cabecera.js
+++ picostrap5-child-base/js/efectos-cabecera.js
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function () {
             clearInterval(interval);
             canvasMenu();
           };
-        }, 1000);
+        }, 400);
       });
     }
     waitUntil("typeof bootstrap != 'undefined'");
@@ -57,7 +57,6 @@ document.addEventListener('DOMContentLoaded', function () {
       generate_menuCanvas('marcas');
       generate_menuCanvas('ecolosofia');
       generate_menuCanvas('contacto');
-      // generate_menuCanvas('menu');
     }
 
     var custom_bootstrap = {};
@@ -69,23 +68,9 @@ document.addEventListener('DOMContentLoaded', function () {
       eval( "var eventMenuOffcanvas = new bootstrap.Offcanvas(menuCanvasMenuOffcanvas);");
 
       eval("linkMenu"+id_element).addEventListener('mouseover', function (event) {
-        // checkAllMenuOffcanvasClose(true);
         ocultarTodosMenus();
-
-
-        // document.querySelector('.offcanvas-' + id_element).classList.remove('opacity-0');
         mostrarMenu(id_element);
         showOffCanvas();
-
-        // eval( "custom_bootstrap."+id_element+" = eventMenuOffcanvas;");
-        // eval("linkMenu"+id_element).classList.add("active","active-offcanvas");
-        // eval("eventMenuOffcanvas").show();
-
-        // eval("menuCanvasMenuOffcanvas").classList.add("menu-open-offcanvas");
-        // eval("menuCanvasMenuOffcanvas").parentElement.classList.remove("h-0");
-        // eval("menuCanvasMenuOffcanvas").classList.remove("h-0");
-        // eval("menuCanvasMenuOffcanvas").classList.add("h-auto");
-
       });
       eval("menuCanvasMenuOffcanvas").addEventListener('show.bs.offcanvas', function () {
         menuContainerColor.classList.add("menu-open-offcanvas");
@@ -98,16 +83,8 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         eval("menuCanvasMenuOffcanvas").classList.remove("h-auto");
         eval("menuCanvasMenuOffcanvas").classList.add("h-0");
-        //menuContainerColor.classList.remove("menu-open-offcanvas");
       });
     }
-
-    // function checkAllMenuOffcanvasClose(d_none){
-    //   ocultarMenu('nosotros');
-    //   ocultarMenu('marcas');
-    //   ocultarMenu('ecolosofia');
-    //   ocultarMenu('contacto');
-    // }
 
     function ocultarTodosMenus() {
       ocultarMenu('nosotros');
@@ -134,8 +111,6 @@ document.addEventListener('DOMContentLoaded', function () {
       let menu = document.querySelector('#offcanvas-menu');
       menu.classList.remove('show');
       menu.classList.remove('menu-open-offcanvas');
-      // menu.classList.remove('visible');
-      
     }
     function showOffCanvas() {
       let topBar = document.querySelector('#wrapper-navbar');
@@ -146,20 +121,6 @@ document.addEventListener('DOMContentLoaded', function () {
       menu.classList.add('visible');
     }
 
-    function checkMenuOffcanvasClose(id_element,d_none){
-      // if(eval("custom_bootstrap."+id_element)){
-      //   if(eval("custom_bootstrap."+id_element+"._isShown")==true){
-      //     if(document.querySelector(".active.active-offcanvas")){
-      //       document.querySelector(".active.active-offcanvas").classList.remove("active");
-      //     }
-      //     if(d_none==true){
-      //       eval("custom_bootstrap."+id_element+"._element.classList.add('d-none');");
-      //     }
-      //     eval("custom_bootstrap."+id_element).hide();
-      //   }
-      // }
-    }
-
     document.querySelector('#wrapper-navbar').addEventListener('mouseleave', function (event) {
       ocultarTodosMenus();
       closeOffCanvas();
@@ -168,7 +129,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const navs_link_not_offcanvas = document.querySelectorAll('.fixed-top .nav-link:not([role="button"]):not(.active)');
     navs_link_not_offcanvas.forEach(function(nav_link_not_offcanvas) {
       nav_link_not_offcanvas.addEventListener('mouseover', function (event) {
-        // checkAllMenuOffcanvasClose(false);
         if(document.querySelector(".active.active-offcanvas")){
           document.querySelector(".active.active-offcanvas").classList.remove("active");
         }
