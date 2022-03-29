d909f19856e507613049a0bcdc20384a582aa0de
david.perez <david.perez@esgalla.com>
2022-03-29 18:34:25 +0200

    area profesional by fields

--- picostrap5-child-base/page-templates/custom-template-area_profesional.php
+++ picostrap5-child-base/page-templates/custom-template-area_profesional.php
@@ -12,7 +12,7 @@ get_header();
 <section>
   <div class="container-fluid position-relative zi-9 bg-black pt-sm-160 pt-120 pb-218 pb-sm-463 py-lg-333">
     <div class="bg-parallax opacity-60 w-100 h-100 position-absolute top-0 start-0" 
-      style="background:url('<?=wp_get_attachment_image_url(790, "full")?>')"></div>
+      style="background:url('<?=wp_get_attachment_image_url($acf_fields['fondo_cabecera'], "full")?>')"></div>
       <div class="container position-relative">
         <div class="row">
           <div class="col-12 col-xl-6">
@@ -36,7 +36,7 @@ get_header();
     <div class="row">
       <div class="col-12 offset-lg-6 col-lg-6 position-relative gx-0 img-version-mobile">
         <div class="bg-custom-image img-parallax py-150 py-sm-254 py-lg-290 w-100 mt-n150 mt-sm-n333 mt-sm-n374 mt-lg-n463" 
-        style="background:url('<?=wp_get_attachment_image_url(927, "full")?>')">
+        style="background:url('<?=wp_get_attachment_image_url($acf_fields['imagen_over_cabecera'], "full")?>')">
         </div>
       </div>
     </div>
@@ -52,14 +52,11 @@ get_header();
               <div class="col-12 col-xl-8 offset-xl-2">
                 <div class="row overflow-hidden">
                   <div class="col-12" data-aos="fade-up">
-                    <div class="h2 fs-24 fs-md-64">Catálogo</div>
+                    <div class="h2 fs-24 fs-md-64">
+                      <?=$acf_fields['titulo_grid_sup_izq']?>
+                    </div>
                     <div class="mt-32 mb-48 mb-lg-0 fs-14 fs-sm-17">
-                      Agua Mineral San Benedetto dispone de un amplísimo catálogo de productos y 
-                      formatos que se adaptan a las necesidades de cada canal de distribución. <br></br>
-                      Tanto en agua mineral como en refrescos, ofrecemos diversidad de formatos idóneos para el <strong>Gran Consumo</strong>, 
-                      con un <i>mix</i> muy atractivo de Producto ―con una excelente calidad organoléptica y nutricional― y Precio, 
-                      manteniéndonos <strong>siempre cerca de los hogares</strong> a través de marcas históricas y accesibles. 
-                      Estamos presentes en las principales cadenas de supermercados e hipermercados. 
+                      <?=$acf_fields['texto_grid_sup_izq']?>
                     </div>
                   </div>
                 </div>
@@ -68,11 +65,11 @@ get_header();
           </div>
           <div class="order-1 col-12 col-lg-6 position-relative gx-0 img-version-mobile">
             <div class="bg-custom-image img-parallax py-150 py-sm-290 w-100" 
-            style="background:url('<?=wp_get_attachment_image_url(788, "full")?>')">
+            style="background:url('<?=wp_get_attachment_image_url($acf_fields['imagen_grid_sup_der'], "full")?>')">
             </div>
           </div>
           <div class="order-3 order-lg-2 col-12 py-lg-0 py-150 py-sm-290 col-lg-6 bg-custom-image img-parallax img-version-mobile" 
-          style="background:url('<?=wp_get_attachment_image_url(789, "full")?>')">
+          style="background:url('<?=wp_get_attachment_image_url($acf_fields['imagen_grid_inf_izq'], "full")?>')">
           </div>
           <div class="order-2 order-lg-3 col-12 col-lg-6 d-flex align-items-center">
             <div class="row">
@@ -80,15 +77,13 @@ get_header();
                 <div class="row overflow-hidden">
                   <div class="col-12 fs-14 fs-sm-17 my-lg-94 mt-56 mb-64" data-aos="fade-up">
                     <div>
-                    Disponemos también de toda una línea de productos que se adecúan perfectamente a las necesidades de la <strong>hostelería,
-                     tiendas tradicionales y vending</strong>, acompañando a los consumidores en esas ocasiones especiales que demandan 
-                     inmediatez, conveniencia o artículos más <i>premium</i>.<br></br>
-                    Además, desde Agua Mineral San Benedetto España <strong>exportamos más de 16 millones de litros anuales</strong> a 
-                    varios países de la Unión Europea y del norte de África. 
+                      <?=$acf_fields['texto_grid_inf_der']?>
                     </div>
-                    <div class="btn btn-primary text-uppercase text-white rounded-0 fs-14 mt-56 fw-500">
-                      Descarga nuestro catálogo
-                    </div>
+                    <a href="<?=$acf_fields['link_catalogo']?>" class="text-reset text-decoration-none">
+                      <div class="btn btn-primary text-uppercase text-white rounded-0 fs-14 mt-56 fw-500">
+                        Descarga nuestro catálogo
+                      </div>
+                    </a>
                   </div>
                 </div>
               </div>
