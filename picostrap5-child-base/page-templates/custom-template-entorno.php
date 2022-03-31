8a4b53cabd55b23b9f36fe205801db8c94644ef5
david.perez <david.perez@esgalla.com>
2022-03-31 16:43:38 +0200

    ajuste page entornos final

--- picostrap5-child-base/page-templates/custom-template-entorno.php
+++ picostrap5-child-base/page-templates/custom-template-entorno.php
@@ -11,9 +11,9 @@ get_header();
 
 <section>
   <div class="container-fluid position-relative zi-9 bg-black py-160 py-lg-261">
-    <div class="bg-custom-image opacity-60 w-100 h-100 position-absolute top-0 start-0" 
+    <div class="background-position-left-mobile bg-custom-image opacity-60 w-100 h-100 position-absolute top-0 start-0" 
       style="background:url('<?=wp_get_attachment_image_url($acf_fields['fondo_cabecera'], "full")?>')"></div>
-      <?=wp_get_attachment_image(779, "full", "", array( 'class' => 'position-absolute top-50 start-50 translate-middle-y zi-99 mw-50vw d-none d-md-block' , 'alt' => '' , 'title' => '') ); ?>
+      <?=wp_get_attachment_image(779, "full", "", array( 'class' => 'position-absolute top-50 start-50 translate-middle-y zi-99 mw-50vw d-none d-lg-block' , 'alt' => '' , 'title' => '') ); ?>
       <div class="container position-relative">
         <div class="row">
           <div class="col-12 offset-xl-4 col-xl-4 text-center">
@@ -36,20 +36,20 @@ get_header();
   </div>
 </section>
 
-<section class="mt-64 mt-sm-80 mt-lg-185 gx-4 gx-sm-12">
+<section class="mt-56 mt-sm-103 mt-lg-185 gx-4 gx-sm-12">
   <div class="container text-secondary">
     <div class="row">
       <div class="col-12 offset-sm-1 col-sm-10">
         <div class="row overflow-hidden">
-          <div class="col-12 text-center h2 fs-32 fs-md-64">
+          <div class="col-12 text-center h2 fs-32 fs-md-64 lh-1 my-0">
             <?=$acf_fields['texto_superior']?>
           </div>
-          <div class="col-12 text-center mt-64 mt-sm-80 fs-14 fs-sm-17" data-aos="fade-up">
+          <div class="col-12 text-center mt-32 mt-sm-70 fs-14 fs-sm-17" data-aos="fade-up">
             <?=$acf_fields['descripcion']?>
           </div>
           <div class="col-12">
             <div class="row text-center overflow-hiden">
-              <div class="col-12 col-sm-4 mt-60" data-aos="fade-up">
+              <div class="col-12 col-m-4 mt-60" data-aos="fade-up">
                 <div class="row">
                   <div class="col-12 d-flex justify-content-center">
                     <span class="rounded-circle border border-1">
@@ -63,7 +63,7 @@ get_header();
                   </div>
                 </div>
               </div>
-              <div class="col-12 col-sm-4 mt-60" data-aos="fade-down">
+              <div class="col-12 col-m-4 mt-60" data-aos="fade-down">
                 <div class="row">
                   <div class="col-12 d-flex justify-content-center">
                     <span class="rounded-circle border border-1">
@@ -77,7 +77,7 @@ get_header();
                   </div>
                 </div>
               </div>
-              <div class="col-12 col-sm-4 mt-60" data-aos="fade-up">
+              <div class="col-12 col-m-4 mt-60" data-aos="fade-up">
                 <div class="row">
                   <div class="col-12 d-flex justify-content-center">
                     <span class="rounded-circle border border-1">
@@ -99,7 +99,7 @@ get_header();
   </div>
 </section>
 
-<section class="mt-64 mt-sm-120 mt-lg-245 mb-64 mb-sm-103 mb-lg-242 position-relative zi-99 gx-4 gx-sm-12">
+<section class="mt-0 mt-sm-120 mt-lg-245 mb-64 mb-sm-103 mb-lg-242 position-relative zi-99 gx-4 gx-sm-12">
   <div class="py-218 py-sm-218 bg-primary-brighter position-absolute top-custom-entorno translate-middle-y w-100">
   </div>
   <div class="container text-secondary">
@@ -112,10 +112,10 @@ get_header();
             <div class="row">
               <div class="text-center text-lg-start col-12 offset-xl-2 col-xl-8 overflow-hidden" data-aos="fade-up">
                 <div class="row">
-                  <div class="col-12 h3 fs-26 fs-md-45 mt-70 mt-lg-0">
+                  <div class="col-12 h3 fs-26 fs-md-45 mt-128 mt-sm-70 mt-lg-0">
                     <?=$acf_fields['titulo_grid_sup_izq']?>
                   </div>
-                  <div class="col-12 my-32 fs-14 fs-sm-17">
+                  <div class="col-12 mt-32 mb-64 my-sm-32 fs-14 fs-sm-17">
                     <?=$acf_fields['texto_grid_sup_izq']?> 
                   </div>
                 </div>
@@ -132,7 +132,7 @@ get_header();
             <div class="row overflow-hidden">
               <div class="text-center text-lg-start col-12 offset-xl-2 col-xl-8 gx-xl-0" data-aos="fade-up">
                 <div class="row">
-                  <div class="col-12 h3 fs-26 fs-md-45 mt-70 mt-lg-0">
+                  <div class="col-12 h3 fs-26 fs-md-45 mt-64 mt-sm-70 mt-lg-0">
                     <?=$acf_fields['titulo_grid_inf_der']?>
                   </div>
                   <div class="col-12 mt-30 fs-14 fs-sm-17">
