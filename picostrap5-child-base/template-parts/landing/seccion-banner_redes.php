d26b0a6449958ee390bd3348273f6e609f9dee44
david.perez <david.perez@esgalla.com>
2022-04-07 11:29:54 +0200

    enlaces redes

--- picostrap5-child-base/template-parts/landing/seccion-banner_redes.php
+++ picostrap5-child-base/template-parts/landing/seccion-banner_redes.php
@@ -10,8 +10,8 @@
                 <div class="fs-24 fw-bold text-secondary text-center text-xl-end pe-xl-55 text-uppercase lh-1"><?= $seccion['titulo'] ?></div>
             </div>
             <div class="col-12 col-lg-6 d-flex flex-wrap justify-content-center align-items-center mt-40 mt-lg-0">
-                <a href="#" class="d-flex align-items-center fs-24 lh-20 text-secondary text-decoration-none px-12 pe-sm-40 mb-24 mb-sm-0"><i class="fa-brands fa-facebook-f fs-16 me-24"></i>FACEBOOK</a>
-                <a href="" class="d-flex align-items-center fs-24 lh-20 text-secondary text-decoration-none px-12 ps-sm-40"><i class="fa-brands fa-instagram fs-18 me-24"></i>INSTAGRAM</a>
+                <a target="_blank" href="https://www.facebook.com/fuenteprimavera.es" class="d-flex align-items-center fs-24 lh-20 text-secondary text-decoration-none px-12 pe-sm-40 mb-24 mb-sm-0"><i class="fa-brands fa-facebook-f fs-16 me-24"></i>FACEBOOK</a>
+                <a target="_blank" href="https://www.instagram.com/fuenteprimavera_es/" class="d-flex align-items-center fs-24 lh-20 text-secondary text-decoration-none px-12 ps-sm-40"><i class="fa-brands fa-instagram fs-18 me-24"></i>INSTAGRAM</a>
             </div>
         </div>
     </div>
