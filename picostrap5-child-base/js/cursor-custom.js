df37bff9aaafca6a7f01062fd6367548c3d7818a
david.perez <david.perez@esgalla.com>
2022-05-05 14:26:23 +0200

    ajustes funcionamiento cursor benedetto

--- picostrap5-child-base/js/cursor-custom.js
+++ picostrap5-child-base/js/cursor-custom.js
@@ -1,13 +1,19 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
+    var enable_cursor=true;
+
     const contenedor_cursor_custom = document.querySelectorAll('.contenedor-cursor-custom');
     contenedor_cursor_custom.forEach(contenedor => {
         contenedor.addEventListener('mousemove', e=>{
+            enable_cursor=false;
+            if(document.querySelector('.new-cursor-custom')){
+                document.querySelector('.new-cursor-custom').remove();
+            }
             var rect = e.target.getBoundingClientRect();
             if(!contenedor.querySelector('.cursor-custom')){
                 var cursor_custom = document.createElement('div');
-                cursor_custom.className = 'cursor-custom';
+                cursor_custom.className = 'cursor-custom opacity-0';
                 if(contenedor.getAttribute("text-cursor-custom")){
                     cursor_custom.innerHTML = '<div>'+contenedor.getAttribute("text-cursor-custom")+'</div>';
                 }else{
@@ -16,39 +22,52 @@ document.addEventListener('DOMContentLoaded', function () {
                 contenedor.appendChild(cursor_custom);
             }
             var cursorVideo = contenedor.querySelector('.cursor-custom');
-            
-            setTimeout(function () {
+            if(cursorVideo.classList.contains('opacity-0')){
+                setTimeout(function () {
+                    cursorVideo.classList.replace('opacity-0','opacity-100');
+                }, 075);
+            };
+            //setTimeout(function () {
                 cursorVideo.style.top = e.clientY - rect.top - 50 + "px";
                 cursorVideo.style.left = e.clientX - rect.left - 50 + "px";
-                cursorVideo.style.display = "block";
-            }, 075);
-
-            //cursorVideo.setAttribute("style", "top: " + (e.clientY - rect.top - 50) + "px; left: " + (e.clientX - rect.left - 50) + "px; display: block;")
+                //cursorVideo.style.opacity = 1;
+            //}, 075);
         });
         contenedor.addEventListener('mouseleave', e=>{
+            enable_cursor=true;
             var cursorVideo = contenedor.querySelector('.cursor-custom');
             if(cursorVideo){
-                cursorVideo.remove();
+                cursorVideo.classList.replace('opacity-100','opacity-0');
+                setTimeout(function () {
+                    cursorVideo.remove();
+                }, 200);
             }
         });
     });
 
-    /*const contenedor_all_html = document.querySelector('html');
-    contenedor_all_html.addEventListener('mousemove', e=>{
-        var rect = e.target.getBoundingClientRect();
+    var last_top_screen = 0;
 
-        if(!contenedor_all_html.querySelector('.new-cursor-custom')){
-            //CREAMOS DIV
-            var div_new_cursor_custom = document.createElement('div');
-            div_new_cursor_custom.className = 'new-cursor-custom';
-            //div_new_cursor_custom.innerHTML = '<div>NEW CURSOR</div>';
-            contenedor_all_html.appendChild(div_new_cursor_custom);
+    const contenedor_all_html = document.querySelector('html');
+    contenedor_all_html.addEventListener('mousemove', e=>{
+        if(enable_cursor==true){
+            if(!contenedor_all_html.querySelector('.new-cursor-custom')){
+                var div_new_cursor_custom = document.createElement('div');
+                div_new_cursor_custom.className = 'new-cursor-custom';
+                contenedor_all_html.appendChild(div_new_cursor_custom);
+            }
+            var newCursorDiv = contenedor_all_html.querySelector('.new-cursor-custom');
+            newCursorDiv.style.top = e.clientY + document.documentElement.scrollTop  +  "px";
+            newCursorDiv.style.left = e.clientX  + "px";
+            newCursorDiv.style.display = "block";
+            last_top_screen = newCursorDiv.getBoundingClientRect().top;
         }
-        
-        var newCursorDiv = contenedor_all_html.querySelector('.new-cursor-custom');
-        newCursorDiv.style.top = e.clientY + document.documentElement.scrollTop  +  "px";
-        newCursorDiv.style.left = e.clientX  + "px";
-        newCursorDiv.style.display = "block";
-    });*/
+    });
+    window.addEventListener('scroll', event => {
+        var newCursorDiv = document.querySelector('html .new-cursor-custom');
+        if(newCursorDiv)
+        {
+            newCursorDiv.style.top = last_top_screen + scrollY + "px";
+        }
+    });
 
 });
